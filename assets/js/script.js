97280de7e61789e69ee80feac23f294b1ed3cb23
johnamdickson <johnamdickson@me.com>
2023-04-07 20:20:41 +0000

    build: add game functions and card flip functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,39 @@ document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementsByTagName("button");
     let gameButton = button[0];
         gameButton.addEventListener("mousedown", function(){
-            console.log("Pressed button")
-            this.style.boxShadow = "none";
+            this.style.boxShadow = "inset 3px 3px 3px 0 rgba(255,255,255,0.7), inset -3px -3px 3px 0 rgba(255,255,255,0.5)";
+            this.style.color = "rgba(186, 221, 233, 0.9)";
+            runGame()
         })
         // How to return styling to CSS: https://stackoverflow.com/questions/3506050/how-to-reset-the-style-properties-to-their-css-defaults-in-javascript
         gameButton.addEventListener("mouseup", function(){
-            console.log("Unclicked!")
             this.style.boxShadow = "";
+            this.style.color = "";
         })
     })
+/**
+ * The main game "loop", called when the start game button is pressed
+ * allowing player one to select two cards.
+ */
+    function runGame() {
+        let cards = document.getElementsByClassName("card");
+      console.log(cards);
+      for (let card of cards) {
+        card.addEventListener("click", flipCard);
+      }
 
-    function buttonPressed() {
-        
+      function flipCard() {
+        this.classList.toggle("flip");
+        console.log("Clicked card");
+      }
+    }
+
+    
+    function incrementScores() {
 
     }
+
+    function calculateWinner() {
+
+    }
+
