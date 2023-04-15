ae0c07c691de6f5454c3361dead1dfb76fa4ed94
johnamdickson <>
2023-04-15 21:32:58 +0000

    fix: rectify scoring bug issue with check for match call passing player 1 instrad of player two in the appropriate case

--- assets/js/script.js
+++ assets/js/script.js
@@ -310,7 +310,7 @@ function checkForMatch(playerNumber, cardsTurned, cardsTurnedInfo) {
       case 2: 
         if (cardsTurnedInfo[0] === cardsTurnedInfo[1]){
           document.getElementById('player-two-score').innerText = ++ playerTwoScore;
-            feedbackMatch(cardsTurnedInfo[0], 1);
+            feedbackMatch(cardsTurnedInfo[0], 2);
         }
        else {
           //  Turn cards back around if they do not match. Time delay to stop this happening straight away.
